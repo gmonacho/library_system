15813f21317e818cdfd6639500864c16c222a355
gmonacho <gaelmonach123@live.fr>
2022-10-30 17:55:53 +0100

    setup(logging): flask configuration

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,28 @@
-from src.infrastructure.api import app
+import flask
+from logging.config import dictConfig
+from src.infrastructure.book.routes import book_api
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "INFO", "handlers": ["wsgi"]},
+    }
+)
+
+app = flask.Flask(__name__)
+
+app.register_blueprint(book_api)
 
 app.run(host="0.0.0.0", debug=True)
