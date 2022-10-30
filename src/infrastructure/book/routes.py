7976d826e1eb86b29823d5d3fd75702e82fcfb2e
gmonacho <gaelmonach123@live.fr>
2022-10-30 17:58:24 +0100

    impl(BookRoutes): responses and typing optimizations

--- src/infrastructure/book/routes.py
+++ src/infrastructure/book/routes.py
@@ -1,13 +1,13 @@
-import flask_pydantic
 import pydantic
+import flask_pydantic
 from flask import Blueprint
-from pydantic import PositiveInt
+from src.domain.utils import Quantity
 from src.application.book.creator import BookCreator
 from src.application.book.deleter import BookDeleter
 from src.application.book.requests import BookCreationRequest, BookDeletionRequest, BookUpdateRequest
 from src.application.book.updater import BookUpdater
-from src.domain.utils import Quantity
 from src.infrastructure.book._ean13 import Ean13
+from src.infrastructure.book.in_memory_repositor import BookInMemoryRepository
 
 book_api = Blueprint("book_api", __name__)
 
@@ -15,13 +15,18 @@ book_api = Blueprint("book_api", __name__)
 class PostBookBody(pydantic.BaseModel):
     barcode: str
     title: str
-    inventory_quantity: PositiveInt
+    inventory_quantity: pydantic.conint(ge=-1)
     summary: str
 
 
+class PostBookResponse(pydantic.BaseModel):
+    pass
+
+
 @book_api.post("/book")
-@flask_pydantic.validate(body=PostBookBody, on_success_status=201)
-def create(body: PostBookBody) -> None:
+@flask_pydantic.validate(on_success_status=201)
+def create(body: PostBookBody) -> PostBookResponse:
+
     try:
         ean = Ean13(body.barcode)
     except ValueError as err:
@@ -33,22 +38,29 @@ def create(body: PostBookBody) -> None:
         inventory_quantity=Quantity(body.inventory_quantity),
         summary=body.summary,
     )
-    BookCreator().create(book_creation_request)
+    BookCreator(BookInMemoryRepository()).create(book_creation_request)
+    return PostBookResponse()
 
 
 class DeleteBookBody(pydantic.BaseModel):
     barcode: str
 
 
+class DeleteBookResponse(pydantic.BaseModel):
+    pass
+
+
 @book_api.delete("/book/<string:barcode>")
 @flask_pydantic.validate(body=DeleteBookBody, on_success_status=200)
-def delete(body: DeleteBookBody) -> None:
+def delete(body: DeleteBookBody) -> DeleteBookResponse:
+
     try:
         ean = Ean13(body.barcode)
     except ValueError as err:
         raise ValueError("barcode format is invalid") from err
     book_deletion_request = BookDeletionRequest(id=ean)
-    BookDeleter().delete(book_deletion_request)
+    BookDeleter(BookInMemoryRepository()).delete(book_deletion_request)
+    return DeleteBookResponse()
 
 
 class UpdateBookBody(pydantic.BaseModel):
@@ -58,9 +70,14 @@ class UpdateBookBody(pydantic.BaseModel):
     summary: str
 
 
+class UpdateBookResponse(pydantic.BaseModel):
+    pass
+
+
 @book_api.put("/book/<string:barcode>")
 @flask_pydantic.validate(body=UpdateBookBody, on_success_status=200)
-def update(body: UpdateBookBody) -> None:
+def update(body: UpdateBookBody) -> UpdateBookResponse:
+
     try:
         ean = Ean13(body.barcode)
     except ValueError as err:
@@ -72,4 +89,5 @@ def update(body: UpdateBookBody) -> None:
         inventory_quantity=Quantity(body.inventory_quantity),
         summary=body.summary,
     )
-    BookUpdater().update(book_update_request)
+    BookUpdater(BookInMemoryRepository()).update(book_update_request)
+    return UpdateBookResponse()
