ac741546bae3ac451f7dda0a31424046a186ecbe
gmonacho <gaelmonach123@live.fr>
2022-10-30 21:12:58 +0100

    add(BookRepository): docstrings

--- src/application/book/ports/repository.py
+++ src/application/book/ports/repository.py
@@ -6,14 +6,29 @@ from src.domain.utils import Id
 class BookRepository(abc.ABC):
     @abc.abstractmethod
     def create(self, book: Book) -> None:
+        """
+        :param book_id:
+        :return:
+        :raise EntityAlreadyExists: if entity already exists
+        """
         pass
 
     @abc.abstractmethod
     def update(self, book: Book) -> None:
+        """
+        :param book_id:
+        :return:
+        :raise CannotRetrieveEntity: if entity of specified id does not exist
+        """
         pass
 
     @abc.abstractmethod
     def delete(self, book_id: Id) -> None:
+        """
+        :param book_id:
+        :return:
+        :raise CannotRetrieveEntity: if entity of specified id does not exist
+        """
         pass
 
     @abc.abstractmethod
